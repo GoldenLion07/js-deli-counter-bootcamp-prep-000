65abcc7834a1f21a46ae28a7fab66ca5d24aab65
gmat191@gmail.com <gmat191@gmail.com>
2018-06-11 20:59:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = []
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return "Welcome" + "you" + 
+  return "Welcome" + name + "you are number" +
 }
 
 function nowServing(person) {
