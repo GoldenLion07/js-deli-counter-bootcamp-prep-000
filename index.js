5721a2d8e4c06d99ad22dedd126305a26388e5c1
gmat191@gmail.com <gmat191@gmail.com>
2018-06-11 21:11:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-var katzDeli = []
+var katzDeliLine = [];
 
+function currentLine(x) {
+    var line = []
+    if (x.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for(var i = 0; i < x.length; i++) {
+        line += (i + 1) + ". " + x[i] + ", "
+      }
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
+    }
+}
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return "Welcome, " + name + "you are number" + katzDeliLine.length + "in line."
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
-
-function nowServing(p) {
-  if (p === 0) { "There is nobody waiting to be served!"} else {
-    var name = p[0];
-    p.splice(0, 1)
-    return "Currently serving " + name + ".";
-  }
-}
-
-function currentLine(p) {
-  let line = []
-  if (p.length === 0){
-    return "The line is currently empty"
+function nowServing(x) {
+  if (x.length === 0) {
+    return "There is nobody waiting to be served!"
   } else {
-    for(var i = 0; i < p.length; i++) {
-      line = line.slice(0, line.length-2)
-      return "the line is currently: " + line
-    }
+    var name = x[0];
+    x.splice(0, 1);
+    return "Currently serving " + name + ".";
   }
 }
